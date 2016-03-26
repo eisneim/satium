6cc70357a032b9244368f41e28a9bcff79203290
eisneim <eisneim1@gmail.com>
2016-03-26 12:00:28 +0800

    [done] vdom/component.js

--- src/vdom/component.js
+++ src/vdom/component.js
@@ -4,6 +4,7 @@ import options from '../options'
 import {EMPTY,SYNC_RENDER, DOM_RENDER, NO_RENDER, EMPTY_BASE} from '../constants'
 import {getNodeProps} from '.'
 import {createComponent,collectComponent} from './component-recycler'
+import { removeOrphanedChildren, recollectNodeTree } from './diff';
 
 const debug = require('debug')('sa:vdom/component')
 
@@ -15,8 +16,9 @@ const debug = require('debug')('sa:vdom/component')
  * @param {Object} context   ..
  */
 export function setComponentProps(component, props, opts=EMPTY, context) {
-  // save rendering state temperarily
+  debug('setComponentProps')
   let d = component._disableRendering
+  // change rendering state temperarily
   // component should not be rendering when changing context and props
   component._disableRendering = true
 
@@ -29,13 +31,14 @@ export function setComponentProps(component, props, opts=EMPTY, context) {
   }
 
   if(component.base) {
+    debug('componentWillReceiveProps',component.base)
     hook(component, 'componentWillReceiveProps', props, component.context)
   }
 
   if(!component.prevProps) 
     component.prevProps = clone(component.props)
   component.props = props
-  // set prop,context is done, restore rendering state back
+  debug('set prop,context is done, restore rendering state back')
   component._disableRendering = d
 
   if(opts.render !== false) {
@@ -60,7 +63,7 @@ export function setComponentProps(component, props, opts=EMPTY, context) {
  */
 export function renderComponent(component, opts={}) {
   if(component._disableRendering) return
-
+  debug('renderComponent')
   let skip,rendered
   // this is acctually the newState/newProp/newContext
   let {props, state, context} = component.state
@@ -71,6 +74,7 @@ export function renderComponent(component, opts={}) {
 
   // just an update operation, domNode is already there, so need update
   if(isUpdate) {
+    debug('just an update',component.base)
     // set currentState/Props to be prev
     component.props = prevProps
     component.state = prevState
@@ -86,7 +90,7 @@ export function renderComponent(component, opts={}) {
     component.context = context
   } // end of update check
 
-  // GC, delete old state/prop/context
+  debug('GC, delete old state/prop/context')
   component.prevProps = component.prevState = component.prevContext = null
   component._dirty = false
 
@@ -102,13 +106,17 @@ export function renderComponent(component, opts={}) {
       toUnmount, 
       // dom node
       base
+    debug('childComponent',childComponent)
 
     if(isFunction(childComponent) && childComponent.prototype.render) {
+      debug('is an component, not a dom node')
       // set up higher order component link
       // !!! this is should named component._childComponent
       let instance = component._component 
+      debug('component._component ',component._component )
       // ??? what does this mean?
       if(instance && instance.constructor !== childComponent) {
+        debug('instance.constructor !== childComponent')
         toUnmount = instance
         instance = null
       }
@@ -116,11 +124,12 @@ export function renderComponent(component, opts={}) {
       let childProps = getNodeProps(node)
 
       if(instance) {
-        // render child 
+        debug('render child ')
         setComponentProps(instance, childProps,SYNC_RENDER, childContext)
 
-        // not instance, get a new one form recycler -------------------------------
+        //  -------------------------------
       } else { 
+        debug('not instance, get a new one form recycler')
         instance = createComponent(childComponent, childProps, childContext)
         // save parent for reference
         instance._parentComponent = component
@@ -130,9 +139,10 @@ export function renderComponent(component, opts={}) {
         // ??? why always check 'component.base' ?
         if(component.base) // should this be 'isUpdate' ??
           deepHook(instance, 'componentWillMount')
-        // no render, but just to trigger "componentWillReceiveProps" for child
+        debug('no render, but just to trigger "componentWillReceiveProps" for child')
         setComponentProps(instance, childProps, NO_RENDER, childContext)
-        // do the actual dom render check
+
+        debug('do the actual dom render check')
         renderComponent(instance, DOM_RENDER)
         
         // ??? why always check 'component.base' ?
@@ -145,9 +155,9 @@ export function renderComponent(component, opts={}) {
     } else { 
       debug('not a function or no render method')
       // ------------ not a function or no render method ======================
-      // ------------ this is a real dom node ? ------------
+      debug('this is HOC? stateless function? a real dom node ?')
       let cbase = component.base
-      // destroy higher order component lik
+      debug('destroy higher order component lik')
       toUnmount = component._component
       if(toUnmount) {
         cbase = component._component = null
@@ -181,14 +191,14 @@ export function renderComponent(component, opts={}) {
       while((t=t._parentComponent)) {
         componentRef = t
       }
-      // now we got top level component ?
+      debug('now we got top level component ?',componentRef)
       base._component = componentRef
       base._componentConstructor = componentRef.constructor
       debug('get topLevel component and save it to base?')
     }
 
     if(isUpdate) {
-      debug('fir up componentDidUpdate')
+      debug('fire up componentDidUpdate')
       hook(component, 'componentDidUpdate', prevProps, prevState, prevContext)
     }
 
@@ -204,9 +214,84 @@ export function renderComponent(component, opts={}) {
   return rendered // the VNode
 }
 
-
+/**
+ * Remove a component from the DOM and recycle it.
+ * @param  {Node} dom       ..
+ * @param  {Object} component ..
+ * @param  {Boolean} remove    ..
+ * @return {void}           ..
+ */
 export function unmountComponent(dom, component, remove) {
+  debug('unmountComponent')
+  hook(component.props, 'ref', null)
+  hook(component, 'componentWillUnmount')
 
+  // recursively tear down & recollect hight-order component children
+  let inner = component._component
+  if(inner) {
+    unmountComponent(dom, inner)
+  }
+
+  let base = component.base
+  if(base) {
+    if(remove !== false) {
+      let p = base.parentNode
+      if(p) p.removeChild(base)
+    }
+    removeOrphaneChildren(base, base.childNodes, true)
+  }
+
+  if(remove) {
+    component._parentComponent = null
+    collectComponent(component)
+  }
+  hook(component, 'componentDidUnmount')
+}
+/**
+ * Instantiate and render a Component, given a VNode whose nodeName is a constructor.
+ * @param  {VNode} vnode   ..
+ * @param  {Node} dom     ..
+ * @param  {Object} context ..
+ * @return {Node}         ..
+ */
+export function createComponentFromVNode(vnode,dom, context) {
+  debug('createComponentFromVNode')
+  let props = getNodeProps(vnode)
+  let component = createComponent(vnode.nodeName, props, context)
+
+  if(dom && !component.base) component.base = dom
+
+  setComponentProps(component, props, NO_RENDER, context)
+  renderComponent(component, DOM_RENDER)
+
+  return component.base
+}
+
+export function buildComponentFromVNode(dom, vnode, context) {
+  debug('buildComponentFromVNode')
+  let c = dom && dom._component,
+    oldDom = dom
+
+  let isOwner = c && dom._componentConstructor === vnode.nodeName
+  while(c && !isOwner && (c=c._parentComponent)) {
+    isOwner = c.constructor === vnode.nodeName
+  }
+
+  if(isOwner) {
+    setComponentProps(c, getNodeProps(vnode), SYNC_RENDER, context)
+    dom = c.base
+  } else {
+    if(c) {
+      unmountComponent(dom,c, true)
+      dom = oldDom = null
+    }
+    dom = createComponentFromVNode(vnode, dom, context)
+    if(oldDom && dom !== oldDom) {
+      recollectNodeTree(oldDom)
+    }
+  }
+
+  return dom
 }
 
 
